127983207ad8077b9a2ff07c3b94bf71cf8a280e
Your Name <egor.lyfar@gmail.com>
2025-09-22 17:30:19 +0800

    Deploy website - based on 884b279498ac41cce33e477b1a66c4fd7da83737

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/egorlyfar/awa_01_spark/awa-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/egorlyfar/awa_01_spark/awa-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/egorlyfar/awa_01_spark/awa-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/egorlyfar/awa_01_spark/awa-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/egorlyfar/awa_01_spark/awa-docs/src/css/custom.css"),
-];
